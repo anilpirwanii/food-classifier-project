39536d7c55c70adb399db061644344fa3c1c9f93
Anil <aka158@sfu.ca>
2024-12-29 03:50:47 -0800

    updated code for paths

--- notebooks/streamlit_app.py
+++ notebooks/streamlit_app.py
@@ -7,16 +7,17 @@ import streamlit.web.server.server
 import numpy as np
 import os
 
-print("PORT:", os.environ.get("PORT", "Default: 8501"))
-
 # Load the model
-model = load_model('./notebooks/food_classifier_model.keras')
+model_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'food_classifier_model.keras')
+model = load_model(model_path)
 
 # Get class labels
 train_datagen = ImageDataGenerator(rescale=1./255)
+train_data_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../data/train')
 train_generator = train_datagen.flow_from_directory(
-    'data/train', target_size=(224, 224), batch_size=32, class_mode='categorical'
+    train_data_path, target_size=(224, 224), batch_size=32, class_mode='categorical'
 )
+
 class_indices = train_generator.class_indices
 class_labels = {v: k for k, v in class_indices.items()}
 
@@ -28,7 +29,7 @@ uploaded_file = st.file_uploader("Choose a food image...", type=["jpg", "jpeg", 
 
 if uploaded_file is not None:
     # Save the uploaded file temporarily
-    temp_path = os.path.join("temp.jpg")
+    temp_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'temp.jpg')
     with open(temp_path, "wb") as f:
         f.write(uploaded_file.getbuffer())
 
