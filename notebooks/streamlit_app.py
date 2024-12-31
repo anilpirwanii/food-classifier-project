9af41e86ba682e6d446be7bbd09506b1bc0ce792
Anil <aka158@sfu.ca>
2024-12-31 00:22:08 -0800

    removed previous changes

--- notebooks/streamlit_app.py
+++ notebooks/streamlit_app.py
@@ -48,9 +48,6 @@ if uploaded_file is not None:
     img_array = img_to_array(img) / 255.0
     img_array = np.expand_dims(img_array, axis=0)
 
-    st.write("Preprocessed Image Array:", img_array)
-    st.write("Loaded Class Labels:", class_labels)
-
     # # Debugging: Print the loaded class labels
     # st.write("Loaded Class Labels:", class_labels)
 
